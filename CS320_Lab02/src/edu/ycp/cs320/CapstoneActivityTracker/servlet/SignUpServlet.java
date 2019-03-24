919cf8c0519c55467949f970d3753f80d693bcac
smashedTuba28 <jsteinberg@ycp.edu>
2019-03-24 16:08:01 -0400

    Updated signin Servlet
    	now uses fakedb

--- CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/servlet/SignUpServlet.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/servlet/SignUpServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+import edu.ycp.cs320.CapstonActivtyTracker.db.FakeDatabase;
 import edu.ycp.cs320.CapstoneActivityTracker.model.*;
 
 public class SignUpServlet extends HttpServlet {
@@ -20,10 +20,29 @@ public class SignUpServlet extends HttpServlet {
 			throws ServletException, IOException {
 		
 		System.out.println("SignUp Servlet: doGet");
+		
+		String email = req.getSession().getAttribute("userEmail").toString();
+		
+		//check session
+		if ( email != null) {
+			FakeDatabase fake = new FakeDatabase();
+			fake.init();
+			
+			//check for faculty or student
+			if(fake.isFaculty(email)) {
+				//redirect faculty to adminView
+				req.getRequestDispatcher("/_view/adminView.jsp").forward(req, resp);
+			}
+			else {
+				//redirect students to studentView
+				req.getRequestDispatcher("/_view/studentView.jsp").forward(req, resp);
+			}
+		}
+		else {
 		//call the jsp and generate empty form
 		req.getRequestDispatcher("/_view/signUp.jsp").forward(req, resp);
+		}
 	}
-	
 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
@@ -33,46 +52,44 @@ public class SignUpServlet extends HttpServlet {
 		
 		//error message String to hold message text when applicable
 		String errorMessage = null;
-		
-		//TODO:
-		//create model
-		//Account model = null;
-		
-		//TODO:
-		//create controller
-		
-		//TODO:
-		//assign model to controller
-		
-		//FOR HARDCODE
 		String firstname = null;
 		String lastname = null;
 		String email = null;
+		String eConfirm = null;
 		String password = null;
 		String passConfirm = null;
 		String schoolID = null;
-		//FOR HARDCODE
+		String faculty = null;
+		Boolean valid = false;
+		
+		FakeDatabase fake = new FakeDatabase();
+		fake.init();
 		
 		//decode POSted from parameter and dispatch to controller
 		try {
-			/*String*/ firstname = req.getParameter("firstname");
-			/*String*/ lastname = req.getParameter("lastname");
-			/*String*/ email = req.getParameter("email");
-			/*String*/ password = req.getParameter("password");
-			/*String*/ passConfirm = req.getParameter("passConfrim");
-			/*String*/ schoolID = req.getParameter("schoolID");
+			firstname = req.getParameter("firstname");
+			lastname = req.getParameter("lastname");
+			email = req.getParameter("email");
+			eConfirm = req.getParameter("emailConfirm");
+			password = req.getParameter("password");
+			passConfirm = req.getParameter("passConfrim");
+			schoolID = req.getParameter("schoolID");
+			faculty = req.getParameter("faculty");
 			//TODO: boolean for faculaty (jsp have a check box or drop down select)
 	 
 			//check that none are empty
 			if (firstname == null || lastname == null || email == null 
-					|| password == null || schoolID == null) {
-				
+					|| password == null || schoolID == null || eConfirm == null
+					|| passConfirm == null || faculty == null) {
 				errorMessage = "All fields required\n";
 			}
 			//all fields have info entered
 			if (!email.endsWith("@ycp.edu")) {
 				//if it is not a ycp email
 				errorMessage = errorMessage + "Enter a valid YCP email\n";	
+			}
+			if(!email.equals(eConfirm)) {
+				errorMessage = errorMessage + "Emails Don't Match\n";
 			}
 			if (password.length() < 8) {
 				errorMessage = errorMessage + "Password must contain at least 8 characters\n";
@@ -84,29 +101,25 @@ public class SignUpServlet extends HttpServlet {
 			
 			if (!schoolID.startsWith("90") || schoolID.length() != 9) {
 				errorMessage = errorMessage + "Invalid School ID #\n";
-			}			
-		}catch (Exception e) {
-			errorMessage = errorMessage + "\nResolve all errors before continuing\n";
-		}
-		
-		
-		boolean valid = true;
-		//data cleared all initial checks if errorMessage is still null
-		if (errorMessage == null) {
-			//TODO: send data to controller to create Account and verify signUp
+			}	
 			
-			/*
-			//HARD CODE EXAMPLE SECTION BEGINS
-			model.setFirstname(firstname);
-			model.setLastname(lastname);
-			model.setEmail(email);
-			model.setSchoolID(schoolID);
-			model.setPassword(password);
-			model.setFacualty(faculty);
-			valid = model.validAccount();
-			//END HARDCODE SECTION
-			*/
+			if(errorMessage == null) {
+			//passed all error checks and data ready to be added to db	
+				if(faculty.equals("0")){//student
+					fake.createAccount(firstname, lastname, email, password, schoolID, false);
+				}
+				else {//admin
+					fake.createAccount(firstname, lastname, email, password, schoolID, true);
+				}
+			}	
+			
+			//make sure creation was successful
+			valid = fake.verifyAccount(email, password);
+			
+		}catch (Exception e) {
+			errorMessage = errorMessage + "\nUNABLE TO CREATE ACCOUNT: CHECK CREDENTIALS AND TRY AGAIN\n";
 		}
+		
 		
 		if (valid == true) {
 			//if account was made
@@ -114,7 +127,6 @@ public class SignUpServlet extends HttpServlet {
 			req.getRequestDispatcher("/_view/signIn.jsp").forward(req, resp);
 		}
 		else {
-		
 		//TODO:set "model" in jsp to reference desired model class from above
 		//req.setAttribute("model", model);
 		//set the errorMessage text to the response
