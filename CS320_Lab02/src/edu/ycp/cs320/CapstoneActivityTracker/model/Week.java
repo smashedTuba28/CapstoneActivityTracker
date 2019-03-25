b1868e144784da7a6ba2f102fc3348f59e0212ec
smashedTuba28 <jsteinberg@ycp.edu>
2019-03-24 21:38:41 -0400

    FakeDB hard coded initializations
    	Times and Logs for
    		Jason
    		Travis
    		William
    		and Robert California

--- CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/model/Week.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/model/Week.java
@@ -2,67 +2,74 @@ package edu.ycp.cs320.CapstoneActivityTracker.model;
 
 public class Week {
 
-	long dur0, dur1, dur2, dur3, dur4, dur5;
+	String dur0, dur1, dur2, dur3, dur4, dur5, dur6;
 	
 	public Week() {
-		
 	}
 	
-	public Week(long dur0, long dur1, long dur2, long dur3, long dur4, long dur5) {
+	public Week(String dur0, String dur1, String dur2, String dur3, String dur4, String dur5, String dur6) {
 		this.dur0 = dur0;
 		this.dur1 = dur1;
 		this.dur2 = dur2;
 		this.dur3 = dur3;
 		this.dur4 = dur4;
 		this.dur5 = dur5;
+		this.dur6 = dur6;
 	}
 	
-	public long getDur0() {
+	public String getDur0() {
 		return dur0;
 	}
 	
-	public long getDur1() {
+	public String getDur1() {
 		return dur1;
 	}
 	
-	public long getDur2() {
+	public String getDur2() {
 		return dur2;
 	}
 	
-	public long getDur3() {
+	public String getDur3() {
 		return dur3;
 	}
 	
-	public long getDur4() {
+	public String getDur4() {
 		return dur4;
 	}
 	
-	public long getDur5() {
+	public String getDur5() {
 		return dur5;
 	}
 	
-	public void setDur0(long dur0) {
+	public String getDur6() {
+		return dur6;
+	}
+	
+	public void setDur0(String dur0) {
 		this.dur0 =  dur0;
 	}
 	
-	public void setDur1(long dur1) {
+	public void setDur1(String dur1) {
 		this.dur1 =  dur1;
 	}
 	
-	public void setDur2(long dur2) {
+	public void setDur2(String dur2) {
 		this.dur2 =  dur2;
 	}
 	
-	public void setDur3(long dur3) {
+	public void setDur3(String dur3) {
 		this.dur3 =  dur3;
 	}
 	
-	public void setDur4(long dur4) {
+	public void setDur4(String dur4) {
 		this.dur4 =  dur0;
 	}
 	
-	public void setDur5(long dur5) {
+	public void setDur5(String dur5) {
 		this.dur5 =  dur5;
 	}
 	
+	public void setDur6(String dur5) {
+		this.dur5 =  dur5;
+	}
 }
