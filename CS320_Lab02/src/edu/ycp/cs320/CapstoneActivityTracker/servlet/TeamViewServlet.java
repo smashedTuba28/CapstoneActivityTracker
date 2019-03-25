8b9e646a38280804bc1d798b5fa0971af10f8d89
smashedTuba28 <jsteinberg@ycp.edu>
2019-03-24 23:03:09 -0400

    Started creating Team View Servlet
    	pushing before rebase with another branch

--- CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/servlet/TeamViewServlet.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/servlet/TeamViewServlet.java
@@ -1,24 +1,68 @@
 package edu.ycp.cs320.CapstoneActivityTracker.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.ycp.cs320.CapstonActivtyTracker.db.FakeDatabase;
+import edu.ycp.cs320.CapstoneActivityTracker.model.Log;
+import edu.ycp.cs320.CapstoneActivityTracker.model.Week;
+
 public class TeamViewServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
 	
-	//retrieve jsp for SignIn
+	//retrieve jsp
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		System.out.println("ForgotPassword Servlet: doGet");
-		//call the jsp and generate empty form
-		req.getRequestDispatcher("/_view/teamView.jsp").forward(req, resp);
-	}
-
+		System.out.println("TeamView Servlet: doGet");
+	
+	
+		String email = (String) req.getSession().getAttribute("userEmail");
+	
+		//check session
+		if ( email == null) {
+			//redirect
+			resp.sendRedirect(req.getContextPath() + "/signIn");
+		}
+		else {	
+			//create fakedb and initialze
+			FakeDatabase fakedb = new FakeDatabase();
+			fakedb.init();
+				
+			List<Week> weekList = fakedb.getAllWeek();
+			List<Log> logList = fakedb.getAllLogs();
+				
+			//get appropriate week and log
+			if(email.equals("jsteinberg@ycp.edu")) {
+				req.setAttribute("week", weekList.get(0));
+				req.setAttribute("log", logList.get(0));
+			}
+			else if(email.equals("twetzel1@ycp.edu")) {
+				req.setAttribute("week", weekList.get(1));
+				req.setAttribute("log", logList.get(1));
+			}
+			else if (email.equals("wtaylor1@ycp.edu")){
+				req.setAttribute("week", weekList.get(2));
+				req.setAttribute("log", logList.get(2));
+			}
+			else if(email.equals("lizardking@ycp.edu")){
+				req.setAttribute("week", weekList.get(3));
+				req.setAttribute("log", logList.get(3));
+			}
+			else {
+				//email not from hard coded list jump back to signIn
+				resp.sendRedirect(req.getContentType() + "/signIn");
+			}
+			//call the jsp and generate empty form
+			req.getRequestDispatcher("/_view/studentView.jsp").forward(req, resp);
+		}
+	}		
 }
+
