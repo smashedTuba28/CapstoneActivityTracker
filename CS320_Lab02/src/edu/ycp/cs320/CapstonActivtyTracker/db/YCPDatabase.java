78c664182375aed19cb0f1f462aedd4891377305
twetzel1 <twetzel1@ycp.edu>
2019-04-01 14:54:32 -0400

    YCPDB update
    YCPFakeDB comments

--- CS320_Lab02/src/edu/ycp/cs320/CapstonActivtyTracker/db/YCPDatabase.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstonActivtyTracker/db/YCPDatabase.java
@@ -3,3 +3,11 @@ package edu.ycp.cs320.CapstonActivtyTracker.db;
 public class YCPDatabase {
 
 }
+/*
+ * TODO:  
+ * make YCPPersonnel
+ * findStudentByEmail()
+ * findRoomByNumber()
+ * findStudentBySchoolID
+ * YCPDB will be hardcoded students, admins, and rooms with swipes
+ */
