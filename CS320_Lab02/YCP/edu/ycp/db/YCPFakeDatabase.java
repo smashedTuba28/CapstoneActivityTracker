2b319b55293f2d226ae043a6674a9f7b2e5cbae9
Travis Wetzel <twetzel1@ycp.edu>
2019-04-07 17:52:35 -0400

    Adding methods (and comments) to YCPFakeDB
    added:
    findStudentByEmail
    findAdminByEmail
    findStudentBySchoolID
    findAdminBySchoolID
    verifyStudent
    verifyAdmin
    findRoomByNumber

--- CS320_Lab02/YCP/edu/ycp/db/YCPFakeDatabase.java
+++ CS320_Lab02/YCP/edu/ycp/db/YCPFakeDatabase.java
@@ -6,15 +6,18 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import edu.ycp.cs320.CapstoneActivityTracker.model.*;
 import edu.ycp.model.*;
 
 public class YCPFakeDatabase {
 	private List<YCPPersonnel> adminList;
 	private List<YCPPersonnel> studentList;
+	private List<Room> roomList;
 	
 	public YCPFakeDatabase() {
 		adminList = new ArrayList<YCPPersonnel>();
 		studentList = new ArrayList<YCPPersonnel>();
+		roomList = new ArrayList<Room>();
 		
 		readInitialData();
 		
@@ -22,8 +25,90 @@ public class YCPFakeDatabase {
 		System.out.println(studentList.size() + " students");
 	}
 	
+	//Initialization of YCPFakeDB through InitYCPDB Methods
 	public void readInitialData() {
 			adminList.addAll(InitYCPDB.getAdmins());
 			studentList.addAll(InitYCPDB.getStudents());
 	}
+
+	//finding student YCPPersonnel by their email
+	public YCPPersonnel findStudentByEmail(String email) {
+		for(YCPPersonnel student: studentList) {
+			if(student.getEmail().equals(email)) {
+				return student;
+			}
+		}
+		return null;
+	}
+
+	//finding student YCPPersonnel by their email
+	public YCPPersonnel findAdminByEmail(String email) {
+		for(YCPPersonnel admin: adminList) {
+			if(admin.getEmail().equals(email)) {
+				return admin;
+			}
+		}
+		return null;
+	}
+	
+	//finding student YCPPersonnel by their schoolID
+	public YCPPersonnel findStudentBySchoolID(String schoolID) {
+		for(YCPPersonnel student: studentList) {
+			if(student.getSchoolID().equals(schoolID)) {
+				return student;
+			}
+		}
+		return null;
+	}
+	
+	//finding admin YCPPersonnel by their schoolID
+	public YCPPersonnel findAdminBySchoolID(String schoolID) {
+		for(YCPPersonnel admin: adminList) {
+			if(admin.getSchoolID().equals(schoolID)) {
+				return admin;
+			}
+		}
+		return null;
+	}
+	
+	//used to check if student exists within the YCPDB
+	public boolean verifyStudent(String email, String schoolID) {
+		for(YCPPersonnel student: studentList) {
+			if(student.getEmail().equals(email) && student.getSchoolID().equals(schoolID)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//used to check if admin exists within the YCPDB
+	public boolean verifyAdmin(String email, String schoolID) {
+		for(YCPPersonnel admin: adminList) {
+			if(admin.getEmail().equals(email) && admin.getSchoolID().equals(schoolID)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	//returns a room from a room number given
+	public Room findRoomByNumber(int number) {
+		for(Room room: roomList) {
+			if(room.getRoomNumber() == number) {
+				return room;
+			}
+		}
+		return null;
+	}
+	
 }
+
+/*
+ * TODO:  
+ * make YCPPersonnel
+ * findStudentByEmail()
+ * findRoomByNumber()
+ * findStudentBySchoolID
+ * YCPDB will be hardcoded students, admins, and rooms with swipes
+ */
+
