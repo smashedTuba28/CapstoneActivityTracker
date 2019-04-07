faeda7b70b5e96a78b7f285da125618c75d19ee9
Travis Wetzel <twetzel1@ycp.edu>
2019-04-07 17:04:11 -0400

    InitYCPDB fixed
    list were not initialized therefore returning null

--- CS320_Lab02/YCP/edu/ycp/db/InitYCPDB.java
+++ CS320_Lab02/YCP/edu/ycp/db/InitYCPDB.java
@@ -1,6 +1,8 @@
 package edu.ycp.db;
 
 import edu.ycp.model.*;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class InitYCPDB {
@@ -9,13 +11,21 @@ public class InitYCPDB {
 	private static List<YCPPersonnel> studentList;
 	
 	public static List<YCPPersonnel> getAdmins(){
+		
+		adminList = new ArrayList<YCPPersonnel>();
+		
 		adminList.add(new YCPPersonnel("John", "Doe", "jdoe@ycp.edu", "900000000", true));
+		
 		return adminList;
 	}
 	public static List<YCPPersonnel> getStudents(){
+		
+		studentList = new ArrayList<YCPPersonnel>();
+		
 		studentList.add(new YCPPersonnel("Jason", "Steinberg", "jsteinerg@ycp.edu", "900000001", false));
 		studentList.add(new YCPPersonnel("Travis", "Wetzel", "twetzel1@ycp.edu", "900000002", false));
 		studentList.add(new YCPPersonnel("William", "Taylor", "wtaylor1@ycp.edu", "900000003", false));
+		
 		return studentList;
 	}
 }
