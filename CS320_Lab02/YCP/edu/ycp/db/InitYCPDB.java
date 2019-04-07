b43c32093342fa5ae8de6f4b09c81324dd9decea
Travis Wetzel <twetzel1@ycp.edu>
2019-04-07 17:05:46 -0400

    test commit & push

--- CS320_Lab02/YCP/edu/ycp/db/InitYCPDB.java
+++ CS320_Lab02/YCP/edu/ycp/db/InitYCPDB.java
@@ -11,17 +11,13 @@ public class InitYCPDB {
 	private static List<YCPPersonnel> studentList;
 	
 	public static List<YCPPersonnel> getAdmins(){
-		
 		adminList = new ArrayList<YCPPersonnel>();
-		
 		adminList.add(new YCPPersonnel("John", "Doe", "jdoe@ycp.edu", "900000000", true));
 		
 		return adminList;
 	}
 	public static List<YCPPersonnel> getStudents(){
-		
 		studentList = new ArrayList<YCPPersonnel>();
-		
 		studentList.add(new YCPPersonnel("Jason", "Steinberg", "jsteinerg@ycp.edu", "900000001", false));
 		studentList.add(new YCPPersonnel("Travis", "Wetzel", "twetzel1@ycp.edu", "900000002", false));
 		studentList.add(new YCPPersonnel("William", "Taylor", "wtaylor1@ycp.edu", "900000003", false));
