5b2b9995cdd368a58f304d0010437ac47da1d6aa
Travis Wetzel <twetzel1@ycp.edu>
2019-04-07 15:36:48 -0400

    Deleted john doe from students

--- CS320_Lab02/YCP/edu/ycp/db/InitYCPDB.java
+++ CS320_Lab02/YCP/edu/ycp/db/InitYCPDB.java
@@ -13,7 +13,6 @@ public class InitYCPDB {
 		return adminList;
 	}
 	public static List<YCPPersonnel> getStudents(){
-		studentList.add(new YCPPersonnel("John", "Doe", "jdoe@ycp.edu", "900000000", false));
 		studentList.add(new YCPPersonnel("Jason", "Steinberg", "jsteinerg@ycp.edu", "900000001", false));
 		studentList.add(new YCPPersonnel("Travis", "Wetzel", "twetzel1@ycp.edu", "900000002", false));
 		studentList.add(new YCPPersonnel("William", "Taylor", "wtaylor1@ycp.edu", "900000003", false));
