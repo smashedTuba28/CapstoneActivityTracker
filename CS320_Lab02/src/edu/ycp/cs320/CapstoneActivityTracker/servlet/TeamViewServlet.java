075701aa5436934f1b9b510f9b04b3f3e1ccb494
smashedTuba28 <jsteinberg@ycp.edu>
2019-04-07 10:24:50 -0400

    TeamView Servlet updates
    	TeamViewServlet now uses MVC instead of hard coded if else 		statements

--- CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/servlet/TeamViewServlet.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/servlet/TeamViewServlet.java
@@ -1,7 +1,9 @@
 package edu.ycp.cs320.CapstoneActivityTracker.servlet;
 
 import java.io.IOException;
+import java.util.Date;
 import java.util.List;
+import com.google.gson.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import edu.ycp.cs320.CapstonActivtyTracker.db.FakeDatabase;
+import edu.ycp.cs320.CapstoneActivityTracker.controller.ChartController;
+import edu.ycp.cs320.CapstoneActivityTracker.model.ChartModel;
 import edu.ycp.cs320.CapstoneActivityTracker.model.Log;
 import edu.ycp.cs320.CapstoneActivityTracker.model.Week;
 
@@ -22,34 +26,29 @@ public class TeamViewServlet extends HttpServlet {
 			throws ServletException, IOException {
 		
 		System.out.println("TeamView Servlet: doGet");
-	
-	
 		String email = (String) req.getSession().getAttribute("userEmail");
-	
+		
 		//check session
 		if ( email == null) {
-			//redirect
-			System.out.println("Here");
+			//redirect to signIn
 			resp.sendRedirect(req.getContextPath() + "/signIn");
 		}
-		else {	
-			//create fakedb and initialze
-			FakeDatabase fakedb = new FakeDatabase();
-			fakedb.init();
-				
-			List<Week> weekList = fakedb.getAllWeek();
-			List<Log> logList = fakedb.getAllLogs();
-				
-			//get appropriate week and logs hard coded in
-				req.setAttribute("week0", weekList.get(0));
-				req.setAttribute("log0", logList.get(0));
-				req.setAttribute("week1", weekList.get(1));
-				req.setAttribute("log1", logList.get(1));
-				req.setAttribute("week2", weekList.get(2));
-				req.setAttribute("log2", logList.get(2));
-				req.setAttribute("week3", weekList.get(3));
-				req.setAttribute("log3", logList.get(3));
 		
+		else {	
+			ChartController controller = new ChartController();//create controller
+			ChartModel model = new ChartModel();//create model
+			controller.setModel(model);//populate model
+			
+			Date end = new Date();
+			end.setHours(24);
+			end.setMinutes(0);
+			end.setSeconds(0);
+			Date start = new Date(end.getTime() - 604800000);
+			
+			//populates model with needed information for jsp
+			controller.populateTopTeamWeek(email, start, end);
+			
+			req.setAttribute("model", model);
 			//call the jsp and generate empty form
 			req.getRequestDispatcher("/_view/teamView.jsp").forward(req, resp);
 		}
