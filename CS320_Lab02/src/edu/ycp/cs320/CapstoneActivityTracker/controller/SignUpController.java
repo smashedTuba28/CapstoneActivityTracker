ce15a2daca620add7c7025a27f14482d58bca4eb
Travis Wetzel <twetzel1@ycp.edu>
2019-04-07 20:13:46 -0400

    SignUpController
    CreateAccount made

--- CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/controller/SignUpController.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/controller/SignUpController.java
@@ -9,17 +9,30 @@ public class SignUpController {
 	
 	public SignUpController() {
 		//initialize databases
+		fdb = new FakeDatabase();
+		ycpdb = new YCPFakeDatabase();
 	}
 	
 	public boolean createAccount(String email, String password, String schoolID) {
-		//use incoming values to get account from YCPDB
-		
-		//if in ycp database !null 
-		
-			//use fdb to createAccount using information from the ycpdb return
-				//if creation successful return true
-		
-		//if not with in ycp database / creation unsuccessful
+		//checking if personnel is within the YCPDB as an admin
+		if(ycpdb.verifyAdmin(email, schoolID)) {
+			fdb.createAccount(ycpdb.findAdminByEmail(email).getFirstname(), ycpdb.findAdminByEmail(email).getLastname(), email, password, schoolID, true);
+			return true;
+		}
+		//checking if personnel is within the YCPDB as a student
+		else if(ycpdb.verifyStudent(email, schoolID)) {
+			fdb.createAccount(ycpdb.findStudentByEmail(email).getFirstname(), ycpdb.findStudentByEmail(email).getLastname(), email, password, schoolID, false);
+			return true;
+		}
+		//if the personnel does not exist, return false as the account has not been created
+		else
 		return false;
 	}
 }
+//use incoming values to get account from YCPDB
+//if in ycp database !null 
+
+	//use fdb to createAccount using information from the ycpdb return
+		//if creation successful return true
+
+//if not with in ycp database / creation unsuccessful
