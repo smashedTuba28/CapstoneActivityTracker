08194cbe30fa78388591438a0ebb7a0fdc6f1227
Travis Wetzel <twetzel1@ycp.edu>
2019-05-11 20:02:48 -0400

    CreateSubTeamController nearly finished

--- CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/controller/CreateSubTeamController.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/controller/CreateSubTeamController.java
@@ -1,8 +1,12 @@
 package edu.ycp.cs320.CapstoneActivityTracker.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.ycp.cs320.CapstonActivtyTracker.db.DatabaseProvider;
 import edu.ycp.cs320.CapstonActivtyTracker.db.DerbyDatabase;
 import edu.ycp.cs320.CapstonActivtyTracker.db.IDatabase;
+import edu.ycp.cs320.CapstoneActivityTracker.model.TopTeam;
 
 public class CreateSubTeamController {
 	private IDatabase db;
