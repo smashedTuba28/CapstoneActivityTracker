c2c7b616f5b8ed907de03e898eff710d50547a88
Travis Wetzel <twetzel1@ycp.edu>
2019-05-11 22:01:51 -0400

    CreateSubTeamController and Servlet
    Initial versions. might need revision

--- CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/controller/CreateSubTeamController.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/controller/CreateSubTeamController.java
@@ -9,17 +9,26 @@ import edu.ycp.cs320.CapstonActivtyTracker.db.IDatabase;
 import edu.ycp.cs320.CapstoneActivityTracker.model.TopTeam;
 
 public class CreateSubTeamController {
+	
 	private IDatabase db;
+	private List<TopTeam> topTeamsList;
 	
 	public CreateSubTeamController() {
 		
 		DatabaseProvider.setInstance(new DerbyDatabase());
 		db = DatabaseProvider.getInstance();
 	}
-	public boolean createSubTeam(String subTeamname, String topTeamID) {
-		if(subTeamname.equals("null") || topTeamID.equals("null")) {
+	public boolean createSubTeam(String subTeamname, String topTeamname) {
+		if(subTeamname.equals("null") || topTeamname.equals("null")) {
 			return false;
 		}
-		return db.createSubTeam(subTeamname, Integer.parseInt(topTeamID));
+		//getting top team id with the given teamname from the servlet
+		int topTeamID = db.getTopTeamWithTeamname(topTeamname).getTeamID();
+		//returning either true or false based on whether the data is valid or not
+		return db.createSubTeam(subTeamname, topTeamID);
 	}
+	public List<TopTeam> getTopTeamsList() {
+		return db.getAllTopTeams();
+	}
+	
 }
--- CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/servlet/CreateSubTeamServlet.java
+++ CS320_Lab02/src/edu/ycp/cs320/CapstoneActivityTracker/servlet/CreateSubTeamServlet.java
@@ -1,11 +1,15 @@
 package edu.ycp.cs320.CapstoneActivityTracker.servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import edu.ycp.cs320.CapstoneActivityTracker.controller.CreateSubTeamController;
+import edu.ycp.cs320.CapstoneActivityTracker.model.StudentAccount;
 
 public class CreateSubTeamServlet extends HttpServlet {
 
@@ -15,10 +19,99 @@ public class CreateSubTeamServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		System.out.println("Index Servlet SignOut: doGet");
+		System.out.println("CreateSubTeam Servlet: doGet");
 		
-		req.getSession().removeAttribute("account_id");
-		req.getSession().removeAttribute("accountType");
-		resp.sendRedirect(req.getContextPath() + "/signIn");
+		String account_id= null;
+		
+		try {
+			account_id = req.getSession().getAttribute("account_id").toString();
+		}
+		catch(NullPointerException e) {
+
+		}
+		//check session
+		if ( account_id == null) {
+			//redirect when the user does not have an account_id
+			resp.sendRedirect(req.getContextPath() + "/signIn");
+		}
+		else {
+			CreateSubTeamController controller = new CreateSubTeamController();
+			req.setAttribute("ListOfTopTeam", controller.getTopTeamsList());
+
+			//call the jsp and generate empty form
+			req.getRequestDispatcher("/_view/createSubTeam.jsp").forward(req, resp);
+			
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException{
+		
+		System.out.println("createSubTeam Servlet: doPost");
+		
+		//create controller
+		CreateSubTeamController controller = new CreateSubTeamController();
+			
+		//error message String to hold message text when applicable
+		String errorMessage = null;
+		String teamname = null;
+		String topTeam = null;
+		
+		
+		boolean TTcreation = false;
+		
+		try {
+			//decode POSted from parameter and dispatch to controller
+			teamname = req.getParameter("teamname").toString();
+			
+		}
+		catch(NullPointerException e) {
+			
+		}
+		try {
+			//decode POSted from parameter and dispatch to controller
+			topTeam = req.getParameter("topTeamname").toString();
+			
+		}
+		catch(NullPointerException e) {
+			
+		}
+		
+		//check that none are empty
+		if (teamname == null ) {
+			errorMessage = "Please Enter a Team Name";
+			System.out.println("teamname==null");
+			req.setAttribute("ListOfTopTeam", controller.getTopTeamsList());
+			req.getRequestDispatcher("/_view/createSubTeam.jsp").forward(req, resp);
+		}
+		
+			if(topTeam=="val") {
+				System.out.println("need to select a TopTeam");
+				errorMessage = "Please select a Capstone Team";
+				req.setAttribute("ListOfTopTeam", controller.getTopTeamsList());
+				req.getRequestDispatcher("/_view/createSubTeam.jsp").forward(req, resp);
+			}
+			
+			else {
+				errorMessage = "Team Successfully Created!";
+				controller.createSubTeam(teamname, topTeam);
+				req.setAttribute("ListOfTopTeam", controller.getTopTeamsList());
+				req.getRequestDispatcher("/_view/createSubTeam.jsp").forward(req, resp);
+			}
+		
+		errorMessage = "Shoot, something went wrong";
+		System.out.println("Failure: Line 93 CreateSubTeamServlet reached");
+		req.getRequestDispatcher("/_view/createSubTeam.jsp").forward(req, resp);
+		
 	}
 }
+
+
+
+
+
+
+
+
+
